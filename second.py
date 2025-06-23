11137b01b118f513f617974c185ec80908b3bbbe
wajeed <shaikhwajeed639@gmail.com>
2025-06-23 11:04:44 +0530

    added the function in second.py and called in index.py

--- second.py
+++ second.py
@@ -1,3 +1,11 @@
 print("this is the second file")
 print("its modified)
 write some udf function for better output 
+
+list=[1,2,4,5,6,7]
+a=0
+for i in range list:
+     i=2 
+print("this is even")
+    
+
